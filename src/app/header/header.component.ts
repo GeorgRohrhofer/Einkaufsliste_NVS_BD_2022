5c8970e1b5811f12aa8c273b0a7d3b950e230a45
GeorgRohrhofer <rohrhofer.georg@student.htlwrn.ac.at>
2022-03-08 20:15:11 +0100

    Element-Count added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
+import { Subscription } from 'rxjs';
+//import * as internal from 'stream';
+import { DataService } from '../data.service';
+import { Element } from '../element';
+
 
 @Component({
   selector: 'app-header',
@@ -6,8 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  elements: Element[] = [];
+  count =  0;
+  subscription: Subscription;
+  constructor(private dataService : DataService) { 
+    this.subscription = dataService.getSubject().subscribe(
+      {
+        next: () => this.dataService.getTodos().subscribe((data: Element[])=> this.count = data.length)
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
